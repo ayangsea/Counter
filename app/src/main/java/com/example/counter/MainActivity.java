953aa9a217c63ee71da6633e5c134ded43be389a
ayangsea <ayang@overlake.org>
2022-02-03 09:06:49 -0800

    logic

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.counter.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private com.example.counter.databinding.ActivityMainBinding mBinding;
+    private int mCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+        mBinding.plus.setOnClickListener(v->updateCounter(1));
+        mBinding.minus.setOnClickListener(v->updateCounter(-1));
+    }
+
+    private void updateCounter(int i) {
+        mCounter += i;
+        mBinding.counter.setText(String.valueOf(mCounter));
     }
 }
